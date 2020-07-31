8e2a6564091db61ce4bf5075ba2d5113fe75a639
kramarkirill <kkramar@kantiana.ru>
2020-07-31 17:38:35 +0200

    Initial Commit

---
+++ numberGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  numberGame
+//
+//  Created by Кирилл Крамар on 31.07.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
